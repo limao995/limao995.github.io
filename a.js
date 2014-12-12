fa7d627d1c6aed91074b589f41413ccfb78df2ed
limao995 <junger.jack@gmail.com>
2014-12-13 00:41:10 +0800

    use Utf8

--- a.js
+++ a.js
@@ -2,10 +2,10 @@ var s;
 var e;
 var r;
 do{
-s = prompt("¶}©l¼Ó¼h","1");
+s = prompt("é–‹å§‹æ¨“å±¤","1");
 }while(isNaN(s)||s<=0);
 do{
-e = prompt("µ²§ô¼Ó¼h","");
+e = prompt("çµæŸæ¨“å±¤","");
 }while(isNaN(e)||e<=0);
 k=parseInt(Math.random()*(e-s+1))+parseInt(s);
 alert(k);
