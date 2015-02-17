e5a411f6c618ed90f205d5f1da698ec7f17b051d
limao995 <junger.jack@gmail.com>
2015-02-17 09:37:39 +0800

    Update a.js

--- a.js
+++ a.js
@@ -1,7 +1,6 @@
 var s;
 var e;
 var r;
-oScript.src="http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js";
 do{
 s = prompt("開始樓層","1");
 }while(isNaN(s)||s<=0);
@@ -10,7 +9,3 @@ e = prompt("結束樓層","");
 }while(isNaN(e)||e<=0);
 k=parseInt(Math.random()*(e-s+1))+parseInt(s);
 alert(k);
-if(window.location.href=="http://guild.gamer.com.tw/singleACMsg.php?sn=*"){
-var sn = "replyMsg" + getUrlVars()["sn"];
-document.getElementById(sn).value="中獎樓層:"+k;
-}
